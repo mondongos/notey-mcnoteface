91bf7bc5e51f7ba3f0788b1ffbb4640b39eee5ec
Max Van Aken <mtvanaken@gmail.com>
2019-11-25 18:04:45 +0000

    Database CRUD for notes route

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -7,6 +7,7 @@ router.route('/').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err))
 })
 
+// Create
 router.route('/add').post((req, res) => {
     const username = req.body.username
     const title = req.body.title
@@ -21,5 +22,34 @@ router.route('/add').post((req, res) => {
     .then(() => res.json('Note added'))
     .catch(err => res.status(400).json('Error: ' + err))
 }) 
+
+// Read
+router.route('/:id').get((req, res) => {
+    Note.findById(req.params.id)
+    .then(note => res.json(note))
+    .catch(err => res.status(400).json('Error: ', + err))
+})
+
+// Update
+router.route('/update/:id').post((req, res) => {
+    Note.findById(req.params.id)
+    .then(note => {
+        note.username = req.body.username, 
+        note.title = req.body.title,
+        note.textbody = req.body.textbody
+
+        note.save()
+        .then(() => res.json('Note updated'))
+        .catch(err => res.status(400).json('Error' + err))
+    })
+
+})
+
+// Delete
+router.route('/:id').delete((req, res) => {
+    Note.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Note Deleted'))
+    .catch(err => res.status(400).json('Error: ' + err)) 
+})
 
 module.exports = router
