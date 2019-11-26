0bcca8302a873a31047f5514d9e3505d6ea35894
Max Van Aken <mtvanaken@gmail.com>
2019-11-26 10:20:38 +0000

    WIP - link create note component with backend

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css'
-
 import Navbar from './components/navbar.component'
 import NotesList from './components/notes-list.component'
 import EditNote from './components/edit-note.component'
--- src/components/create-note.component.js
+++ src/components/create-note.component.js
@@ -10,33 +10,27 @@ export default class CreateNote extends React.Component {
             users: []
         }
     }
-
-    onChangeTitle(e) {
+    
+    handleChange = (e) => {
+        const {name, value} = e.target
         this.setState({
-            username: e.target.value
+            [name]: value
         })
     }
 
-    onChangeTextBody(e) {
-        this.setState({
-            title: e.target.value
-        })
+    postNote = async (note) => {
+        let rawResponse = await fetch('http://localhost:5000/notes/add', note)
+        return rawResponse
     }
 
-    onChangeUsername(e) {
-        this.setState({
-            textbody: e.target.value
-        })
-    }
-
-    onSubmit(e) {
+    onSubmit = (e) => {
         e.preventDefault()
         const note = {
             username: this.state.username,
             title: this.state.title,
             textbody: this.state.textbody
         }
-
+        this.postNote(note)
         console.log(note)
         window.location = '/'
     }
@@ -44,7 +38,35 @@ export default class CreateNote extends React.Component {
     render() {
         return(
             <div>
-                Create Note
+                <h3>Create new note</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group"> 
+                        <input  
+                            type="text"
+                            required
+                            name='title'
+                            className="form-control"
+                            placeholder="Note title"
+                            value={this.state.title}
+                            onChange={this.handleChange}/>
+                    </div>
+                    <div className="form-group"> 
+                        <input  
+                            type="text"
+                            required
+                            name='textbody'
+                            className="form-control"
+                            placeholder="Type your note here"
+                            value={this.state.textbody}
+                            onChange={this.handleChange}/>
+                    </div>
+                    <div className="form-group">
+                        <input 
+                            type="submit" 
+                            value="Create Note" 
+                            className="btn btn-primary"/>
+                    </div>
+                </form>
             </div>
         )
     }
